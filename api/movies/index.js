0e986d00122b3f3440ae0c263d7bc82c607f77b8
BruceWan1021 <brucewan1021@foxmail.com>
2024-12-19 21:29:04 +0000

    Authentication lab - exercise (genres)

--- api/movies/index.js
+++ api/movies/index.js
@@ -2,7 +2,7 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import { getUpcomingMovies } from '../tmdb-api';
-  
+import { getMovieGenres } from '../tmdb-api';
 
 const router = express.Router();
 
@@ -41,6 +41,13 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+//Get movies genres
+router.get('/tmdb/genres/:movie_id', asyncHandler(async (req, res) => {
+    const { movie_id } = req.params;
+    const movieGenres = await getMovieGenres(movie_id);
+    res.status(200).json(movieGenres);
 }));
 
 export default router;
